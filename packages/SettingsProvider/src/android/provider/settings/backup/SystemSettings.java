8e2edbc27602836f1c8c3649a867f05e3c3312ff
Ido Ben-Hur <idoybh2@gmail.com>
2022-01-10 17:17:27 +0200

    SystemUI: Allow toggling floating rotation button [1/2]
    Change-Id: If51f57b197264f9615e3492bc1574a7003e1d82e
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -105,5 +105,6 @@ public class SystemSettings {
         Settings.System.SCREEN_FLASH_NOTIFICATION_COLOR,
         Settings.System.RINGTONE_VIBRATION_PATTERN,
         Settings.System.NOTIFICATION_VIBRATION_PATTERN,
+        Settings.System.ENABLE_FLOATING_ROTATION_BUTTON,
     };
 }
--- packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
@@ -233,5 +233,6 @@ public class SystemSettingsValidators {
         VALIDATORS.put(System.SCREEN_FLASH_NOTIFICATION_COLOR, ANY_INTEGER_VALIDATOR);
         VALIDATORS.put(System.RINGTONE_VIBRATION_PATTERN, new InclusiveIntegerRangeValidator(0, 4));
         VALIDATORS.put(System.NOTIFICATION_VIBRATION_PATTERN, new InclusiveIntegerRangeValidator(0, 4));
+        VALIDATORS.put(System.ENABLE_FLOATING_ROTATION_BUTTON, BOOLEAN_VALIDATOR);
     }
 }
