22c934e0b7def43ab5d3fcfcb6ac56960becbda9
Rishawn <70256146+Stealth1226@users.noreply.github.com>
2022-01-10 09:47:15 +0530

    SystemUI: Allow disabling ripple effect on unlock [1/2]
    neobuddy89:
    * Reverse logic. Enabled animation by default.
    
    Squashed:
    
    Author: Nauval Rizky <enuma.alrizky@gmail.com>
    Date:   Wed Jan 19 08:31:56 2022 +0700
    
        SystemUI: Fix method for disabling unlock ripple animation
    
        NotificationShadeWindowController's are not gracefully closed, which
        leads to top parts of the screen is being obscured by invisible shade window.
        And also, "ripple" is actually still exists due to lightRevealScrim are
        being started by onKeyguardFadingAwayChanged, which is not what user expect
        when they want to disable the effect.
    
        So in order to handle those cases, lets intercept it from the
        AuthRippleController instead.
    
        [palaych] With system settings we need to pass UserHandle.USER_CURRENT to make sure we are getting proper value.
    
        Change-Id: I85ab96368c8b0099700f3de18ba95ce63eaac443
    
    Co-authored-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Change-Id: Idecaa59547159d0a2b0ecdb55a207377bd466710
    [ghostrider-reborn: use more appropriate setting name]
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5671,6 +5671,12 @@ public final class Settings {
         public static final String NOTIFICATION_SOUND_VIB_SCREEN_ON = "notification_sound_vib_screen_on";
 
         /**
+         * Whether to show the ripple animation on fingerprint unlock
+         * @hide
+         */
+        public static final String AUTH_RIPPLE_ENABLED = "auth_ripple_enabled";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
