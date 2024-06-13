3eb9368cd377ad33917b1ce79bb37950b2634e48
Dhina17 <dhinalogu@gmail.com>
2024-06-13 16:29:18 +0530

    GameManagerService: Fix gamespace on secondary users
    Change-Id: I80c23dbfe900253510d027c891b2e6f441c45132
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- services/core/java/com/android/server/app/GameManagerService.java
+++ services/core/java/com/android/server/app/GameManagerService.java
@@ -2247,8 +2247,9 @@ public final class GameManagerService extends IGameManagerService.Stub {
         @Override
         public void onChange(boolean selfChange, Uri uri) {
             synchronized (mDeviceConfigLock) {
-                String newValue = Settings.Secure.getString(mContentResolver,
-                        Settings.Secure.GAME_OVERLAY);
+                String newValue = Settings.Secure.getStringForUser(mContentResolver,
+                        Settings.Secure.GAME_OVERLAY, UserHandle.USER_CURRENT);
+                if (newValue == null) return;
                 // We write key and value of the device_config property as a single string
                 // from our GameSpace.
                 // ';;' is the separator betweeen key and value.
