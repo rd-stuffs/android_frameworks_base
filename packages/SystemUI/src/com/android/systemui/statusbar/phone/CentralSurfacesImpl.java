58ae20e04f9a009f09c2aa841c54a62c4da1b556
Richard Raya <rdxzv.dev@gmail.com>
2025-01-30 18:21:49 -0300

    Revert "SystemUI: Disable animations during biometric wake-and-unlock"
    This reverts commit 43ae0da6ce55f5ed04a277148ce31ecaf625d895.
    
    Change-Id: I36ee12b1258773a84f6821a643f2a0a1a259517b
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -2794,9 +2794,6 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
 
         mScrimController.setExpansionAffectsAlpha(!unlocking);
 
-        mScrimController.setLaunchingAffordanceWithPreview(
-                mBiometricUnlockController.isWakeAndUnlock());
-
         if (mAlternateBouncerInteractor.isVisibleState()) {
             if (DeviceEntryUdfpsRefactor.isEnabled()) {
                 if ((!mKeyguardStateController.isOccluded() || mShadeSurface.isPanelExpanded())
